acd97cbe820fc5e1a88f0daf0e36c85182c92bdd
tim goh <me@timothygoh.org>
2022-03-26 15:32:04 +0800

    a

--- picker.lua
+++ picker.lua
@@ -5,42 +5,52 @@ local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
+local vga_files = {}
+-- for line in io.lines(".bookmarks") do
+--     table.insert(vga_files, line)
+-- end
+
+local test = vim.fn.glob(".bookmarks")
+if test ~= "" then
+    -- print(vim.inspect("test is" .. test))
+    -- print(vim.inspect("hello"))
+    print(vim.inspect("cwd"))
+    for line in io.lines(".bookmarks") do
+        table.insert(vga_files, line)
+    end
+end
+
 -- our picker function: colors
 local colors = function(opts)
     opts = opts or {}
     pickers.new(opts, {
         prompt_title = "colors",
         -- finder = finders.new_table {results = {"red", "green", "blue"}},
+        finder = finders.new_table {results = vga_files},
 
-    finder = finders.new_table {
-      results = {
-        { "redd", "#ff0000" },
-        { "green", "#00ff00" },
-        { "blue", "#0000ff" },
-      },
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry[1],
-          ordinal = entry[1],
-        }
-      end
-    },
+        -- finder = finders.new_table {
+        --     results = {
+        --         {"redd", "#ff0000"}, {"green", "#00ff00"}, {"blue", "#0000ff"}
+        --     },
+        --     entry_maker = function(entry)
+        --         return {value = entry, display = entry[1], ordinal = entry[1]}
+        --     end
+        -- },
 
-        sorter = conf.generic_sorter(opts),
+        sorter = conf.generic_sorter(opts)
 
-        attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        print(vim.inspect(selection))
-        vim.api.nvim_put({ selection.value[1] }, "", false, true)
-      end)
-      return true
-    end,
+        -- attach_mappings = function(prompt_bufnr, map)
+        --     actions.select_default:replace(function()
+        --         actions.close(prompt_bufnr)
+        --         local selection = action_state.get_selected_entry()
+        --         print(vim.inspect(selection))
+        --         vim.api.nvim_put({selection.value[2]}, "", false, true)
+        --     end)
+        --     return true
+        -- end
     }):find()
 end
 
 -- to execute the function
--- colors()
-colors(require("telescope.themes").get_dropdown{})
+colors()
+-- colors(require("telescope.themes").get_dropdown {})
